8d0f567be5c792e84df0ee756ef4be2a5e2534d7
void <git.email.address@googlemail.com>
2011-04-30 02:19:26 +0200

    The player handling is still pretty broken

--- www/application/PlayerHandler.rb
+++ www/application/PlayerHandler.rb
@@ -16,19 +16,32 @@ class PlayerHandler < SiteContainer
   end
 
   def viewPlayer(request)
-    playerIdString = request.argument.first
+    playerIdString = request.arguments.first
     if !playerIdString.isNumber
       argumentError
     end
     playerId = playerIdString.to_i
-    result = @database[:player_result].select(:summoner_name).where(player_id: playerId).limit(1).all
+    result = @database[:player_result].select(:summoner_name).where(user_id: playerId).limit(1).all
     if result.empty?
       argumentError
     end
     playerName = result.first[:summoner_name]
     title = playerName
-    playerData = nil
-    content = renderPlayer(playerData)
+    #select count(*) from game_result where exists(select * from team_player, player_result where (team_player.team_id = game_result.defeated_team_id or team_player.team_id = game_result.victorious_team_id) and player_result.user_id = xxx);
+    #gameResults = @database[:game_result].filter { @database[:team_player].join(:player_result).filter { {team_player__team_id: :game_result__defeated_team_id, team_player__team_id: :game_result__victorious_team_id}.sql_or & {:player_result__user_id: playerId} }.exists }
+    gameResults = @database[:game_result].select(:defeated_team_id, :victorious_team_id).filter(@database.from(:team_player, :player_result).filter(team_player__team_id: :game_result__defeated_team_id).or(team_player__team_id: :game_result__victorious_team_id).and(player_result__user_id: playerId).exists).all
+    defeats = getPlayerPerformance(playerId, gameResults, :defeated_team_id)
+    victories = getPlayerPerformance(playerId, gameResults, :victorious_team_id)
+    content = renderPlayer(playerName, defeats, victories)
     return @generator.get(content, request, title)
+  end
+
+  def getPlayerPerformance(playerId, gameResults, teamSymbol)
+    output = []
+    gameResults.each do |gameResult|
+      teamId = gameResult[teamSymbol]
+      output += @database[:team_player].filter(team_id: teamId).and(player_id: playerId).join(:player_result, user_id: :player_id).all
+    end
+    return output
   end
 end
--- www/visual/PlayerHandler.rb
+++ www/visual/PlayerHandler.rb
@@ -3,8 +3,17 @@ require 'www-library/HTMLWriter'
 require 'application/SiteContainer'
 
 class PlayerHandler < SiteContainer
-  def renderPlayer(playerData)
+  def renderPlayer(playerName, defeats, victories)
     writer = WWWLib::HTMLWriter.new
+    gameCount = defeats.size + victories.size
+    winRatio = sprintf('%.1f', victories.size.to_f / gameCount * 100.0)
+    writer.ul do
+      writer.li { "Name: #{playerName}" }
+      writer.li { "Number of victories: #{victories.size}" }
+      writer.li { "Number of defeats: #{defeats.size}" }
+      writer.li { "Total number of games recorded: #{gameCount}" }
+      writer.li { "Win ratio: #{winRatio}" }
+    end
     return writer.output
   end
 end
